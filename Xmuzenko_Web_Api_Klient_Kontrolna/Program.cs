5c84ff10f4a9d13c16ef64bbc07563fa093ad181
Hmiz_yr05@student.itstep.org <Hmiz_yr05@student.itstep.org>
2021-10-17 13:28:35 +0300

    TDL-3

--- Xmuzenko_Web_Api_Klient_Kontrolna/Program.cs
+++ Xmuzenko_Web_Api_Klient_Kontrolna/Program.cs
@@ -9,7 +9,7 @@ namespace Xmuzenko_Web_Api_Klient_Kontrolna
 {
     static class Program
     {
-        // test123 re
+        // test123 retor
         public const string Url = "http://localhost:34562/api/";
         /// <summary>
         ///  The main entry point for the application.
